cd8f08331f6247f3ad1a7638b12e08705457cb64
chuchip <chuchip@gmail.com>
2019-03-01 14:49:38 +0100

    Pequeños cambios para mejorar el perfil test

--- src/test/java/com/profesorp/profiletest/ProfilesTestsTest.java
+++ src/test/java/com/profesorp/profiletest/ProfilesTestsTest.java
@@ -1,6 +1,7 @@
 package com.profesorp.profiletest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,10 +26,17 @@ public class ProfilesTestsTest {
 	@Autowired 
 	IRead read;
 	
+	@Autowired
+	Environment env;
+	
 	@Test
 	public void inicio() {
 		String name=read.readRegistry(id);
 		assertEquals(name,"test");
+		String principal=env.getProperty("app.principal","NULL");
+		assertNotEquals(principal,"profe"); // No deberia estar la variable app.principal pues estamos en test
+		String test=env.getProperty("app.test","NULL");
+		assertEquals(test,"test");
 	}
 
 }
